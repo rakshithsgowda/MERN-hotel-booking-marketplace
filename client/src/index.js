e029f782cacd3f91c4d7bb35e44d47809f26bd58
rakshith <rakshit.s.gowda@gmail.com>
2021-10-21 19:30:38 +0530

    accessing redux state

--- client/src/index.js
+++ client/src/index.js
@@ -5,27 +5,13 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 // 1. import react-redux and redux.
-import { createStore, combineReducers } from 'redux'
+import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import rootReducer from './reducers'
 
 // 2. create user reducer function.
-const authReducer = (state = {}, action) => {
-  switch (action.type) {
-    case 'LOGGED_IN_USER':
-      return { ...state, ...action.payload }
-    case 'LOGOUT':
-      return action.payload
-    default:
-      return state
-  }
-}
-
 // 3. combine multiple reducers.
-
-const rootReducer = combineReducers({
-  user: authReducer,
-})
 // 4. create redux store
 const store = createStore(rootReducer, composeWithDevTools())
 
